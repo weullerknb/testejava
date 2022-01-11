a8be4d7acead2e7b59fb819112c8f0b2f94822db
Weuller Oliveira <weullerwww@gmail.com>
2022-01-11 09:32:40 -0300

    Projeto criado

---
+++ src/primeiroPrograma/main.java
@@ -0,0 +1,18 @@
+package primeiroPrograma;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		int x, y;
+		
+		x = 5;
+		y = 10;
+		
+		int soma = x + y;
+		
+		System.out.println(soma);
+
+	}
+
+}
